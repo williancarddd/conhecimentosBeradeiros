d12b72f34bf09781cdf403e16f0658ed46297622
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-10-27 10:44:59 -0400

    first api acesss

--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -1,41 +1,61 @@
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { TextInput, Dimensions, SafeAreaView, StatusBar, View, Image, Text, KeyboardAvoidingView, Platform, TouchableOpacity, StyleSheet, SectionList } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-const botIcon = require('../../assets/favicon.png')
-import { Ionicons } from '@expo/vector-icons';
-import { messageListData } from '../api/fakeApi';
-import { groupBy } from 'lodash';
-import { format } from 'date-fns';
-import { pt } from 'date-fns/locale';
-import { IMessage } from '../interfaces/IMessage';
-import uuid from 'react-native-uuid'
-import { colors } from '../utils/colors';
-import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import {
+  TextInput,
+  Dimensions,
+  SafeAreaView,
+  StatusBar,
+  View,
+  Image,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+  StyleSheet,
+  SectionList,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+const botIcon = require("../../assets/favicon.png");
+import { Ionicons } from "@expo/vector-icons";
+import { messageListData } from "../api/fakeApi";
+import { groupBy } from "lodash";
+import { format } from "date-fns";
+import { pt } from "date-fns/locale";
+import { IMessage } from "../interfaces/IMessage";
+import uuid from "react-native-uuid";
+import { colors } from "../utils/colors";
+import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 
-const { width } = Dimensions.get('window');
+const { width } = Dimensions.get("window");
 interface IniciarProps {
-  navigation: BottomTabNavigationProp<Record<string, object | undefined>, string>;
+  navigation: BottomTabNavigationProp<
+    Record<string, object | undefined>,
+    string
+  >;
 }
 
 export function Chat({ navigation }: IniciarProps) {
   const sectionListRef = useRef(null);
-  const [messages, setMessages] = useState<{ title: string; data: IMessage[]; }[]>([]);
+  const [messages, setMessages] = useState<
+    { title: string; data: IMessage[] }[]
+  >([]);
   const [messageList, setMessageList] = useState<IMessage[]>(messageListData);
   const [sections, setSections] = useState<IMessage[][]>([]);
   const [msg, setMsg] = useState("");
 
   function backScreen() {
-    navigation.navigate('Iniciar')
+    navigation.navigate("Iniciar");
   }
 
   useEffect(() => {
-    const groupedList = Object.values(groupBy(messageList, (message) => {
-      return message.createdAt.toISOString().substring(0, 10);
-    }));
-    var data:{ title: string; data: IMessage[]; }[] = [];
+    const groupedList = Object.values(
+      groupBy(messageList, (message) => {
+        return message.createdAt.toISOString().substring(0, 10);
+      }),
+    );
+    var data: { title: string; data: IMessage[] }[] = [];
     groupedList.map((date) => {
-      let sec =  {
-        title: format(new Date(date[0].createdAt), 'PPP', { locale: pt }),
+      let sec = {
+        title: format(new Date(date[0].createdAt), "PPP", { locale: pt }),
         data: [...date],
       };
       data.push(sec);
@@ -43,11 +63,11 @@ export function Chat({ navigation }: IniciarProps) {
     setMessages(data);
     setSections(groupedList);
   }, [messageList]);
-  
+
   function renderMsg(item: IMessage) {
     const isFromUser = item.from === 1;
-    const messageTime = format(item.createdAt, 'HH:mm', { locale: pt });
-  
+    const messageTime = format(item.createdAt, "HH:mm", { locale: pt });
+
     return (
       <View style={isFromUser ? styles.formMe : styles.fromMe}>
         <Text style={styles.msgTxt}>{item.message}</Text>
@@ -57,35 +77,83 @@ export function Chat({ navigation }: IniciarProps) {
       </View>
     );
   }
-  
-  
 
-  function sendMessage() {
+  async function sendMessage() {
     const dateSent = new Date();
     const messageSent = msg;
-  
-    if (messageSent.trim() !== '') {
+    const comunidade = "Comunidade de Nazaré";
+
+    if (messageSent.trim() !== "") {
       const formatMessage: IMessage = {
         id: uuid.v4() as string,
         createdAt: dateSent, // Set the message's creation time as a Date
         message: messageSent,
         status: 1,
         from: 2,
-        to: 1
+        to: 1,
       };
-      setMessageList(prev => [...prev, formatMessage]);
+      setMessageList((prev) => [...prev, formatMessage]);
       setMsg("");
-      //@ts-ignore
+    }
+
+    const getAnswerFromAPI = async () => {
+      try {
+        const response = await fetch("http://192.168.100.175:8000/perguntar", {
+          method: "POST",
+          body: JSON.stringify({
+            question: messageSent,
+            comunidade: comunidade,
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        });
+        const json = await response.json();
+        console.log(json);
+        return json;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const response = await getAnswerFromAPI();
+
+    if (response.sentence.trim() !== "") {
+      const responseMessage: IMessage = {
+        id: uuid.v4() as string,
+        createdAt: dateSent, // Set the message's creation time as a Date
+        message: response.sentence,
+        status: 2,
+        from: 1,
+        to: 2,
+      };
+      setMessageList((prev) => [...prev, responseMessage]);
+      setMsg("");
+    } else {
+      const responseMessage: IMessage = {
+        id: uuid.v4() as string,
+        createdAt: dateSent, // Set the message's creation time as a Date
+        message:
+          "Não foi possivel processar sua pergunta, por favor tente novamente mais tarde.",
+        status: 2,
+        from: 1,
+        to: 2,
+      };
+      setMessageList((prev) => [...prev, responseMessage]);
+      setMsg("");
     }
   }
 
- 
-
   return (
-    <SafeAreaView style={styles.container} >
-      <StatusBar barStyle='light-content' />
-      <View style={styles.header} >
-        <FontAwesome name='chevron-left' size={24} color="white" onPress={backScreen} />
+    <SafeAreaView style={styles.container}>
+      <StatusBar barStyle="light-content" />
+      <View style={styles.header}>
+        <FontAwesome
+          name="chevron-left"
+          size={24}
+          color="white"
+          onPress={backScreen}
+        />
         <Image source={botIcon} style={styles.avatar} />
         <View>
           <Text style={styles.name}> Roberinho </Text>
@@ -97,80 +165,96 @@ export function Chat({ navigation }: IniciarProps) {
         <SectionList
           sections={messages}
           ref={sectionListRef}
-          keyExtractor={item => String(item.id)}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => renderMsg(item)}
           scrollsToTop={false}
           onContentSizeChange={(w, h) => {
             // @ts-ignore
             try {
-              sectionListRef?.current?.scrollToLocation({itemIndex: messageList?.length , sectionIndex: sections[sections?.length]?.length, viewOffset:  -h})
+              sectionListRef?.current?.scrollToLocation({
+                itemIndex: messageList?.length,
+                sectionIndex: sections[sections?.length]?.length,
+                viewOffset: -h,
+              });
             } catch {
-              console.error('error ao scrollar')
+              console.error("error ao scrollar");
             }
           }}
-          renderSectionHeader={({ section: { title } }) =>
+          renderSectionHeader={({ section: { title } }) => (
             <View style={styles.titleContainer}>
               <Text style={styles.sText}>{title}</Text>
-            </View>}
-        
+            </View>
+          )}
         />
       </View>
       <KeyboardAvoidingView
-        behavior='padding'
-        enabled={Platform.OS == 'ios'}
-        style={styles.footer}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', marginRight: 20 }}>
-          <TextInput style={styles.input}  value={msg} onChangeText={setMsg} />
+        behavior="padding"
+        enabled={Platform.OS == "ios"}
+        style={styles.footer}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            marginRight: 20,
+          }}
+        >
+          <TextInput style={styles.input} value={msg} onChangeText={setMsg} />
           <TouchableOpacity>
-            <Ionicons name="send" size={26} color='#ffff' onPress={sendMessage}/>
+            <Ionicons
+              name="send"
+              size={26}
+              color="#ffff"
+              onPress={sendMessage}
+            />
           </TouchableOpacity>
         </View>
       </KeyboardAvoidingView>
     </SafeAreaView>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: colors['deep-fir'][500],
-    borderColor: 'red',
-    flex: 1, 
+    backgroundColor: colors["deep-fir"][500],
+    borderColor: "red",
+    flex: 1,
   },
   footer: {
-    borderTopColor: '#4444447f',
-    borderTopWidth: 1
+    borderTopColor: "#4444447f",
+    borderTopWidth: 1,
   },
   avatar: {
     height: 32,
     width: 32,
     borderRadius: 22,
-    marginHorizontal: 15
+    marginHorizontal: 15,
   },
   header: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     paddingVertical: 7,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
   name: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   status: {
-    color: 'white',
+    color: "white",
     fontSize: 13,
-    padding: 4
+    padding: 4,
   },
   content: {
-    backgroundColor: colors['deep-fir'][500],
-    flex: 1
+    backgroundColor: colors["deep-fir"][500],
+    flex: 1,
   },
   input: {
     height: 40,
-    borderColor: '#4444447f',
-    backgroundColor: colors['deep-fir'][400],
-    color: '#ffff',
+    borderColor: "#4444447f",
+    backgroundColor: colors["deep-fir"][400],
+    color: "#ffff",
     borderWidth: 1,
     borderRadius: 40,
     marginHorizontal: 20,
@@ -178,48 +262,48 @@ const styles = StyleSheet.create({
     paddingLeft: 12,
     flex: 1,
     fontSize: 16,
-    padding: 2
+    padding: 2,
   },
   titleContainer: {
-    alignSelf: 'center',
+    alignSelf: "center",
     marginTop: 12,
     paddingVertical: 3,
     paddingHorizontal: 12,
-    borderRadius: 12
+    borderRadius: 12,
   },
   sText: {
     fontSize: 13,
-    color: 'white',
-    textAlign: 'center'
+    color: "white",
+    textAlign: "center",
   },
   formMe: {
-    backgroundColor: colors['great-blue'][600],
+    backgroundColor: colors["great-blue"][600],
     padding: 10,
     maxWidth: width * 0.64,
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     borderBottomRightRadius: 8,
-    margin: 10
+    margin: 10,
   },
   fromMe: {
-    backgroundColor: colors['great-blue'][700],
+    backgroundColor: colors["great-blue"][700],
     padding: 10,
     maxWidth: width * 0.8,
     borderTopLeftRadius: 8,
     borderTopRightRadius: 8,
     borderBottomLeftRadius: 8,
-    marginTop:  width * 0.20 - 50,
-    marginBottom:  width * 0.20 - 50,
-    marginLeft:  width * 0.20 + 60,
+    marginTop: width * 0.2 - 50,
+    marginBottom: width * 0.2 - 50,
+    marginLeft: width * 0.2 + 60,
   },
   msgTxt: {
     fontSize: 16,
-    color: 'white'
+    color: "white",
   },
   hour: {
     fontSize: 11,
-    color: '#ffffff9e',
-    textAlign: 'right'
-  }
-})
+    color: "#ffffff9e",
+    textAlign: "right",
+  },
+});
 export default Chat;
