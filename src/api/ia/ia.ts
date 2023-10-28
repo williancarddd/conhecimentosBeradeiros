728b0c0ad04e163acf6416cdaccce9fe5b7d9bfa
Ricardo Alves da Silva <ricardcpu@gmail.com>
2023-10-28 08:36:35 -0400

    update axios url

--- src/api/ia/ia.ts
+++ src/api/ia/ia.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const iaAPI = axios.create({
-  baseURL:
-    "https://b2c1-2804-d59-9140-4e00-c7a3-b913-c9c4-3889.ngrok-free.app/",
+  baseURL: "http://golden-shepherd-unique.ngrok-free.app",
 });
